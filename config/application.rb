5082100d1908ee45f227b6568c3173c672b28a79
chris kinoti <devkinoti@gmail.com>
2013-07-03 13:54:08 +0300

    changed the application.rb

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Babel
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    #checking for precompile on heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
